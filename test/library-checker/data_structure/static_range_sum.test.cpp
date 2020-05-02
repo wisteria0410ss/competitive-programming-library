49f745b705eee7799891bdda2a7be70295d2d20e
wisteria0410ss <wisteria0410ss@gmail.com>
2020-05-02 20:39:45 +0900

    微修正

--- test/library-checker/data_structure/static_range_sum.test.cpp
+++ test/library-checker/data_structure/static_range_sum.test.cpp
@@ -9,7 +9,7 @@ int main(){
     int64_t a[500000];
     cin >> n >> q;
     for(int i=0;i<n;++i) cin >> a[i];
-    disjoint_sparse_table dst{n, a, [](auto x, auto y){ return x + y; }};
+    disjoint_sparse_table dst{n, a, [](auto x, auto y)->int64_t{ return x + y; }};
 
     while(q--){
         int l, r;
